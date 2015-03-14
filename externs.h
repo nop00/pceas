2bb37e974b5655a0ac0d0ba029e6f02595dd2ff1
Vincent Cruz <mooz@blockos.org>
2015-03-14 22:15:01 +0100

    Reworked hex number output.

--- externs.h
+++ externs.h
@@ -62,7 +62,6 @@ extern struct t_symbol *lablptr;	/* label pointer into symbol table */
 extern struct t_symbol *glablptr;	/* pointer to the latest defined global symbol */
 extern struct t_symbol *lastlabl;	/* last label we have seen */
 extern struct t_symbol *bank_glabl[4][256];	/* latest global label in each bank */
-extern char hex[];		/* hexadecimal character buffer */
 extern int  stop_pass;	/* stop the program; set by fatal_error() */
 extern int  errcnt;		/* error counter */
 extern void (*opproc)(int *);	/* instruction gen proc */
